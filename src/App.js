3bc41219c3c727cbfcc57a291568e9ff487603ac
hafiz1379 <hafizrasa1379@gmail.com>
2023-08-29 15:57:48 +0430

    Add RocketsPage

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Header from './components/header';
+import RocketsPage from './pages/rockets';
 import Missions from './pages/missions';
 
 function App() {
@@ -8,10 +9,10 @@ function App() {
     <BrowserRouter>
       <Header />
       <Routes>
+        <Route path="/" element={<RocketsPage />} />
         <Route path="/missions" element={<Missions />} />
       </Routes>
     </BrowserRouter>
-
   );
 }
 
