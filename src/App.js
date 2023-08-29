7781c024ea447f37915f02c9978bc4dff4ecc15a
hafiz1379 <hafizrasa1379@gmail.com>
2023-08-29 17:12:26 +0430

    Integrate rocket data fetching into App component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
-import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { Route, Routes } from 'react-router-dom';
 import Header from './components/header';
 import RocketsPage from './pages/rockets';
+import { fetchRockets } from './redux/rockets/rockets-slice';
 import Missions from './pages/missions';
 
 function App() {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchRockets());
+  }, [dispatch]);
+
   return (
-    <BrowserRouter>
-      <Header />
-      <Routes>
-        <Route path="/" element={<RocketsPage />} />
-        <Route path="/missions" element={<Missions />} />
-      </Routes>
-    </BrowserRouter>
+    <>
+      <header>
+        <Header />
+      </header>
+      <main className="container my-3">
+        <Routes>
+          <Route path="/" element={<RocketsPage />} />
+          <Route path="/missions" element={<Missions />} />
+        </Routes>
+      </main>
+    </>
   );
 }
 
