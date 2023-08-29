529a52714eb2364897886ff23260dba1d58a4b80
hafiz1379 <hafizrasa1379@gmail.com>
2023-08-29 17:14:02 +0430

    Expand store configuration for multiple slices

--- src/redux/config-store.js
+++ src/redux/config-store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import missionReducer from './missions/missions-slice';
+import rocketsReducer from './rockets/rockets-slice';
 
 const store = configureStore({
   reducer: {
     missions: missionReducer,
+    rockets: rocketsReducer,
   },
 });
 
