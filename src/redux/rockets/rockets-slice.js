8f666e68627328a16acb7771607dc859f90b85a0
hafiz1379 <hafizrasa1379@gmail.com>
2023-08-30 17:12:25 +0430

    Add rocket reservation/cancellation/filtering

--- src/redux/rockets/rockets-slice.js
+++ src/redux/rockets/rockets-slice.js
@@ -1,26 +1,39 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
-export const fetchRockets = createAsyncThunk('rockets/fetchRockets', async () => {
-  try {
-    const response = await axios.get('https://api.spacexdata.com/v3/rockets');
-    return response.data.map(({
-      id, rocket_name: name, description, flickr_images: flickrImages,
-    }) => ({
-      id,
-      name,
-      description,
-      flickrImages,
-      reserved: false,
-    }));
-  } catch (error) {
-    return error;
-  }
-});
+export const fetchRockets = createAsyncThunk(
+  'rockets/fetchRockets',
+  async () => {
+    try {
+      const response = await axios.get('https://api.spacexdata.com/v3/rockets');
+      return response.data.map(
+        ({
+          id,
+          rocket_name: name,
+          description,
+          flickr_images: flickrImages,
+        }) => ({
+          id,
+          name,
+          description,
+          flickrImages,
+          reserved: false,
+        }),
+      );
+    } catch (error) {
+      return error;
+    }
+  },
+);
 
 export const rocketsSlice = createSlice({
   name: 'rockets',
-  initialState: { rockets: [], loading: false, error: null },
+  initialState: {
+    rockets: [],
+    filteredRockets: [],
+    loading: false,
+    error: null,
+  },
   reducers: {
     reserveRocket: (state, action) => {
       const { payload: rocketId } = action;
@@ -31,11 +44,25 @@ export const rocketsSlice = createSlice({
     },
     cancelRocket: (state, action) => {
       const { payload: rocketId } = action;
-      const rocket = state.rockets.find((rocket) => rocket.id === rocketId);
-      if (rocket) {
-        rocket.reserved = false;
-      }
+      const updatedRockets = state.rockets.map((rocket) => {
+        if (rocket.id === rocketId) {
+          return {
+            ...rocket,
+            reserved: false,
+          };
+        }
+        return rocket;
+      });
+      return {
+        ...state,
+        rockets: updatedRockets,
+        filteredRockets: updatedRockets.filter((rocket) => rocket.reserved),
+      };
     },
+    filterReservedRockets: (state) => ({
+      ...state,
+      filteredRockets: state.rockets.filter((rocket) => rocket.reserved),
+    }),
   },
   extraReducers: (builder) => {
     builder
@@ -57,6 +84,6 @@ export const rocketsSlice = createSlice({
   },
 });
 
-export const { reserveRocket, cancelRocket } = rocketsSlice.actions;
+export const { reserveRocket, cancelRocket, filterReservedRockets } = rocketsSlice.actions;
 
 export default rocketsSlice.reducer;
